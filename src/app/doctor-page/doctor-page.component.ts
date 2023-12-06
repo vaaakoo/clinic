3b788e99ab0f8d64ba0a6d250cb5f2c9eb0cde39
v.janikashvili <vjanikashvili@gmail.com>
2023-12-06 22:55:55 +0400

    დღეისთვის სულ ეს არის

--- src/app/doctor-page/doctor-page.component.ts
+++ src/app/doctor-page/doctor-page.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-doctor-page',
   templateUrl: './doctor-page.component.html',
   styleUrls: ['./doctor-page.component.css']
 })
-export class DoctorPageComponent {
+export class DoctorPageComponent  {
 
+  rows: number[][] = Array.from({ length: 10 }, () => Array(8).fill(0));
 }
